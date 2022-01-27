6f1cf016baa3cb6ff85cc68876c14f4bf0805447
Willytecheira <willingtonsosa@hotmail.com>
2022-01-27 15:56:55 -0300

    Commit inicial

--- app/src/main/java/com/example/ejercicio2/MainActivity.java
+++ app/src/main/java/com/example/ejercicio2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ejercicio2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText e1 = (EditText) findViewById(R.id.nomb);
+        EditText e2 = (EditText) findViewById(R.id.cel);
+        EditText e3 = (EditText) findViewById(R.id.mail);
+        EditText e4 = (EditText) findViewById(R.id.pass);
+        EditText e5 = (EditText) findViewById(R.id.men);
+
+
+        Button b =(Button) findViewById(R.id.bt1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText("");
+                e2.setText("");
+                e3.setText("");
+                e4.setText("");
+                e5.setText("");
+
+                Toast.makeText(getApplicationContext(),"Mensaje Enviado",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
